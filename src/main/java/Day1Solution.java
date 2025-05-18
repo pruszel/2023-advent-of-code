a5cbd96db8ff4737964bd6c95f5b75a624e5dad3
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-05-18 09:59:51 -0700

    Add solution for day 1 part 2

--- src/main/java/Day1Solution.java
+++ src/main/java/Day1Solution.java
@@ -1,26 +1,46 @@
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Day1Solution extends AdventOfCodeSolution {
+    private static final Map<String, Integer> numericWordsToDigits = Map.of(
+            "zero", 0,
+            "one", 1,
+            "two", 2,
+            "three", 3,
+            "four", 4,
+            "five", 5,
+            "six", 6,
+            "seven", 7,
+            "eight", 8,
+            "nine", 9
+    );
 
     public static void main(String[] args) {
         String input = readInputFile("src/Day1Input.txt");
-        ArrayList<Integer> calibrationValues = getCalibrationValues(input);
+
+        // Part 1
+        ArrayList<Integer> calibrationValues = getCalibrationValuesUsingDigitsOnly(input);
         Integer sumCalibrationValues = calibrationValues.stream().reduce(0, Integer::sum);
         System.out.println(sumCalibrationValues);
+
+        // Part 2
+        ArrayList<Integer> calibrationValuesUsingDigitsAndWords = getCalibrationValuesUsingDigitsAndWords(input);
+        Integer sumCalibrationValuesUsingDigitsAndWords = calibrationValuesUsingDigitsAndWords.stream().reduce(0, Integer::sum);
+        System.out.println(sumCalibrationValuesUsingDigitsAndWords);
     }
 
-    private static ArrayList<Integer> getCalibrationValues(String input) {
-        ArrayList<Integer> calibrationValues = new ArrayList<Integer>();
+    private static ArrayList<Integer> getCalibrationValuesUsingDigitsOnly(String input) {
+        ArrayList<Integer> calibrationValues = new ArrayList<>();
 
         // Iterate through each line of the input
         String[] lines = input.split("\n");
         for (String line : lines) {
             // Check if the line contains a number
             if (line.matches(".*\\d.*")) {
-                int firstNumberFromLeft = getFirstNumberFromLeft(line);
-                int firstNumberFromRight = getFirstNumberFromRight(line);
+                IndexAndInteger firstDigitFromLeft = getFirstDigitFromLeft(line);
+                IndexAndInteger firstDigitFromRight = getFirstDigitFromRight(line);
                 // Make integer by combining the two numbers
-                int combinedNumber = Integer.parseInt(String.valueOf(firstNumberFromLeft) + String.valueOf(firstNumberFromRight));
+                int combinedNumber = firstDigitFromLeft.integer() * 10 + firstDigitFromRight.integer();
                 calibrationValues.add(combinedNumber);
             }
         }
@@ -28,19 +48,89 @@ public class Day1Solution extends AdventOfCodeSolution {
         return calibrationValues;
     }
 
-    private static int getFirstNumberFromLeft(String str) {
-        int index = 0;
-        while (str.length() > 0 && !Character.isDigit(str.charAt(index))) {
-            index++;
+    private static ArrayList<Integer> getCalibrationValuesUsingDigitsAndWords(String input) {
+        ArrayList<Integer> calibrationValues = new ArrayList<>();
+
+        // Iterate through each line of the input
+        String[] lines = input.split("\n");
+        for (String line : lines) {
+            IndexAndInteger firstNumericWordFromLeft = getFirstNumericWordFromLeft(line);
+            IndexAndInteger firstNumericWordFromRight = getFirstNumericWordFromRight(line);
+
+            IndexAndInteger firstDigitFromLeft = getFirstDigitFromLeft(line);
+            IndexAndInteger firstDigitFromRight = getFirstDigitFromRight(line);
+
+            IndexAndInteger firstNumberFromLeft;
+            boolean wordComesBeforeDigit = firstNumericWordFromLeft != null && firstDigitFromLeft != null && firstNumericWordFromLeft.index() < firstDigitFromLeft.index();
+            if (wordComesBeforeDigit) {
+                firstNumberFromLeft = firstNumericWordFromLeft;
+            } else if(firstDigitFromLeft != null) {
+                firstNumberFromLeft = firstDigitFromLeft;
+            } else {
+                continue;
+            }
+
+            IndexAndInteger firstNumberFromRight;
+            boolean wordComesAfterDigit = firstNumericWordFromRight != null && firstDigitFromRight != null && firstNumericWordFromRight.index() > firstDigitFromRight.index();
+            if (wordComesAfterDigit) {
+                firstNumberFromRight = firstNumericWordFromRight;
+            } else if(firstDigitFromRight != null) {
+                firstNumberFromRight = firstDigitFromRight;
+            } else {
+                continue;
+            }
+
+            // Make integer by combining the two numbers
+            int combinedNumber = firstNumberFromLeft.integer() * 10 + firstNumberFromRight.integer();
+            calibrationValues.add(combinedNumber);
         }
-        return Integer.parseInt(str.substring(index, index + 1));
+
+        return calibrationValues;
     }
 
-    private static int getFirstNumberFromRight(String str) {
+    private static IndexAndInteger getFirstDigitFromLeft(String str) {
+        int index = 0;
+        while (!str.isEmpty() && !Character.isDigit(str.charAt(index))) {
+            index++;
+        }
+        return new IndexAndInteger(index, Integer.parseInt(str.substring(index, index + 1)));
+    }
+
+    private static IndexAndInteger getFirstDigitFromRight(String str) {
         int index = str.length() - 1;
         while (index >= 0 && !Character.isDigit(str.charAt(index))) {
             index--;
         }
-        return Integer.parseInt(str.substring(index, index + 1));
+        return new IndexAndInteger(index, Integer.parseInt(str.substring(index, index + 1)));
+    }
+
+    private static IndexAndInteger getFirstNumericWordFromLeft(String str) {
+        IndexAndInteger first = null;
+        for (Map.Entry<String, Integer> entry : numericWordsToDigits.entrySet()) {
+            String word = entry.getKey();
+            int index = str.indexOf(word);
+            if (index != -1 && (first == null || index < first.index())) {
+                first = new IndexAndInteger(index, entry.getValue());
+            }
+        }
+        return first;
+    }
+
+    /**
+     * @param str String to search for numeric words
+     * @return IndexAndInteger object or null if no numeric word is found
+     */
+    private static IndexAndInteger getFirstNumericWordFromRight(String str) {
+        IndexAndInteger first = null;
+        for (Map.Entry<String, Integer> entry : numericWordsToDigits.entrySet()) {
+            String word = entry.getKey();
+            int index = str.lastIndexOf(word);
+            if (index != -1 && (first == null || index > first.index())) {
+                first = new IndexAndInteger(index, entry.getValue());
+            }
+        }
+        return first;
     }
 }
+
+record IndexAndInteger(int index, int integer) {}
--- src/test/java/Day1SolutionTest.java
+++ src/test/java/Day1SolutionTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayOutputStream;
@@ -8,7 +7,7 @@ import java.io.PrintStream;
 public class Day1SolutionTest {
 
     @Test
-    void mainPrintsCorrectSum() {
+    void mainPrintsCorrectSumForPart1() {
         // Save the original System.out
         PrintStream originalOut = System.out;
 
@@ -21,6 +20,26 @@ public class Day1SolutionTest {
 
             String printedOutput = outputStream.toString().trim();
             assertTrue(printedOutput.contains("53974"));
+        } finally {
+            // Restore original System.out
+            System.setOut(originalOut);
+        }
+    }
+
+    @Test
+    void mainPrintsCorrectSumForPart2() {
+        // Save the original System.out
+        PrintStream originalOut = System.out;
+
+        try {
+            // Set up a new output stream to capture printed output
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
+
+            Day1Solution.main(new String[]{});
+
+            String printedOutput = outputStream.toString().trim();
+            assertTrue(printedOutput.contains("52840"));
         } finally {
             // Restore original System.out
             System.setOut(originalOut);
