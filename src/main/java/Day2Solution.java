ce3fe9e836cf583b2c0a80deef90c4150a24f133
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-05-19 08:12:30 -0700

    Add solution for day 2 part 1

---
+++ src/main/java/Day2Solution.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day2Solution extends AdventOfCodeSolution {
+    private static final HashMap<String, Integer> colorToLimit = new HashMap<>(
+            Map.of(
+                    "red", 12,
+                    "green", 13,
+                    "blue", 14
+            )
+    );
+
+    public static void main(String[] args) {
+        String input = readInputFile("src/main/resources/Day2Input.txt");
+        ArrayList<Game> validGames = new ArrayList<>();
+
+        // Iterate through each game represented by a line of input
+        String[] lines = input.split("\n");
+        for (String line : lines) {
+            // If true, add the game to set the set of valid games
+            boolean isValid = true;
+
+            // first, chop off first part with Game ID
+            // e.g. "Game 1: 3 blue, 4 red; 2 green" -> "3 blue, 4 red; 2 green"
+            String cubesPart = line.split(":")[1].trim();
+
+            // Get list of cube sets revealed denoted by semicolon
+            // e.g. "3 blue, 4 red; 2 green" -> ["3 blue, 4 red", "2 green"]
+            String[] listOfCubeSets = cubesPart.split(";");
+
+            // Check each cube set to determine if the game is valid
+            cubeSetsLoop:
+            for (String cubeSet: listOfCubeSets) {
+
+                // Get each number of cubes revealed for each color
+                // e.g. "3 blue, 4 red" -> ["3 blue", "4 red"]
+                String[] cubes = cubeSet.split(",");
+
+                // Determine if game is valid by checking if each number of cubes exceeds the limit for that color
+                for (String cube : cubes) {
+                    String color = cube.trim().split(" ")[1];
+                    int number = Integer.parseInt(cube.trim().split(" ")[0]);
+                    if (number > colorToLimit.get(color)) {
+                        isValid = false;
+                        break cubeSetsLoop;
+                    }
+                }
+            }
+
+
+            if (isValid) {
+                // Game ID is between "Game " and ":"
+                int gameId = Integer.parseInt(line.split(":")[0].split(" ")[1]);
+                Game game = new Game(gameId);
+                validGames.add(game);
+            }
+        }
+
+        int gameIdSum = validGames.stream().reduce(0, (sum, game) -> sum + game.id, Integer::sum);
+        System.out.println(gameIdSum);
+    }
+}
+
+class Game {
+    public final int id;
+
+    public Game(int id) {
+        this.id = id;
+    }
+}
---
+++ src/test/java/Day2SolutionTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Day2SolutionTest {
+
+    @Test
+    void mainPrintsCorrectSumForPart1() {
+        // Save the original System.out
+        PrintStream originalOut = System.out;
+
+        try {
+            // Set up a new output stream to capture printed output
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outputStream));
+
+            Day2Solution.main(new String[]{});
+
+            String printedOutput = outputStream.toString().trim();
+            assertTrue(printedOutput.contains("1734"));
+        } finally {
+            // Restore original System.out
+            System.setOut(originalOut);
+        }
+    }
+}
